9cb55d10378ba993a61b13473e1269a78e9022dd
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2021-11-03 14:57:19 +0000

    Commenting of PcoCameraStateMachine

--- data/frameReceiver/include/PcoCameraStateMachine.h
+++ data/frameReceiver/include/PcoCameraStateMachine.h
@@ -1,3 +1,10 @@
+/*
+ * PcoCameraStateMachine.h - finite state machine implementation for the PCO camera controller
+ *
+ * Created on: 22 Sept 2021
+ *     Author: Tim Nicholls, STFC Detector Systems Group
+ */
+
 #ifndef INCLUDE_PCOCAMERASTATEMACHINE_H_
 #define INCLUDE_PCOCAMERASTATEMACHINE_H_
 
@@ -19,6 +26,8 @@ namespace mpl = boost::mpl;
 namespace FrameReceiver
 {
 
+    //! PcoCameraStateException - simple exception class for the state machine, implementing
+    //! an information message accessible via the what() method
     class PcoCameraStateException : public std::exception
     {
     public:
@@ -49,6 +58,8 @@ namespace FrameReceiver
 
     }; // PcoCameraStateException
 
+    //! PcoCameraStateUnknownCommand - PCO camera state machine exception thrown when an
+    //! unrecognised state transition command name is requested
     class PcoCameraStateUnknownCommand : public PcoCameraStateException
     {
     public:
@@ -56,6 +67,8 @@ namespace FrameReceiver
         PcoCameraStateUnknownCommand(const std::string& what) : PcoCameraStateException(what) {};
     };
 
+    //! PcoCameraStateIllegalTransition - PCO camera state machine exception thrown when a state
+    //! transition is requested from a state where it is not valid
     class PcoCameraStateIllegalTransition : public PcoCameraStateException
     {
     public:
@@ -63,103 +76,159 @@ namespace FrameReceiver
         PcoCameraStateIllegalTransition(const std::string& what) : PcoCameraStateException(what) {};
     };
 
-    // Definition of state machine events
-    struct EventConnect : sc::event<EventConnect> {};
-    struct EventDisconnect : sc::event<EventDisconnect> {};
-    struct EventArm : sc::event<EventArm> {};
-    struct EventDisarm : sc::event<EventDisarm> {};
-    struct EventRecordStart : sc::event<EventRecordStart> {} ;
-    struct EventRecordStop : sc::event<EventRecordStop> {} ;
+    //! Definition of state machine events
+    struct EventConnect : sc::event<EventConnect> {};         //!< Camera connect event
+    struct EventDisconnect : sc::event<EventDisconnect> {};   //!< Camera disconnect event
+    struct EventArm : sc::event<EventArm> {};                 //!< Camera arm event
+    struct EventDisarm : sc::event<EventDisarm> {};           //!< Camera disarm event
+    struct EventRecordStart : sc::event<EventRecordStart> {}; //!< Camera recording start event
+    struct EventRecordStop : sc::event<EventRecordStop> {} ;  //!< Camera recording stop event
 
-    // Forward declaration of states
+    //! Forward declaration of states
     struct Disconnected;
     struct Connected;
     struct Armed;
     struct Recording;
 
+    // Forward declaration of the controller class
     class PcoCameraLinkController;
 
+    //! PcoCameraState - state machine for the PCO camera controller
     struct PcoCameraState : sc::state_machine<PcoCameraState, Disconnected>
     {
-        PcoCameraState(PcoCameraLinkController* controller);
-        void unconsumed_event(const sc::event_base& e);
-
+        //! State transition command type enumeration
         enum CommandType
         {
-            CommandUnknown = -1,
-            CommandConnect,
-            CommandDisconnect,
-            CommandArm,
-            CommandDisarm,
-            CommandStartRecording,
-            CommandStopRecording
-        };
-        enum StateType
-        {
-            StateUnknown = -1,
-            StateDisconnected,
-            StateConnected,
-            StateArmed,
-            StateRecording
+            CommandUnknown = -1,   //!< Unknown event
+            CommandConnect,        //!< Connect event
+            CommandDisconnect,     //!< Disconnect event
+            CommandArm,            //!< Arm event
+            CommandDisarm,         //!< Disarm event
+            CommandStartRecording, //!< Start recording event
+            CommandStopRecording   //!< Stop recording event
         };
 
+        //! State type enumeration
+        enum StateType
+        {
+            StateUnknown = -1,  //!< Unknown state
+            StateDisconnected,  //!< Disconnected state
+            StateConnected,     //!< Connected state
+            StateArmed,         //!< Armed state
+            StateRecording      //!< Recording state
+        };
+
+        //! Command type map type definition
+        typedef boost::bimap<std::string, CommandType> CommandTypeMap;
+        //! Command type map entry type definition
+        typedef CommandTypeMap::value_type CommandTypeMapEntry;
+        //! State type map type definition
+        typedef boost::bimap<std::string, StateType> StateTypeMap;
+        //! State type map entry type definition
+        typedef StateTypeMap::value_type StateTypeMapEntry;
+
+        //! Constructor for the PcoCameraState class
+        PcoCameraState(PcoCameraLinkController* controller);
+
+        //! Executes a state transition command
         void execute_command(const char* command);
         void execute_command(std::string& command);
         void execute_command(CommandType command);
 
+        //! Handles unconsumed, i.e. invalid, state transition events
+        void unconsumed_event(const sc::event_base& event);
+
+        //! Maps a command name string to a command type value
         CommandType map_command_to_type(std::string& command);
+
+        //! Maps a state type to a state name
         std::string map_state_to_name(StateType state_type);
 
+        //! Returns the current state name
         std::string current_state_name(void);
+
+        //! Returns the current state type
         StateType current_state(void);
 
-        typedef boost::bimap<std::string, CommandType> CommandTypeMap;
-        typedef CommandTypeMap::value_type CommandTypeMapEntry;
-        typedef boost::bimap<std::string, StateType> StateTypeMap;
-        typedef StateTypeMap::value_type StateTypeMapEntry;
-
+        //! Initialises the command type map
         void init_command_type_map(void);
+
+        //! Initialises the state type map
         void init_state_type_map(void);
 
-        PcoCameraLinkController* controller_;
-        CommandTypeMap command_type_map_;
-        StateTypeMap state_type_map_;
-        boost::mutex state_transition_mutex_;
+        PcoCameraLinkController* controller_; //!< Pointer to controller instance
+        CommandTypeMap command_type_map_;     //!< Command type map
+        StateTypeMap state_type_map_;         //!< State type map
+        boost::mutex state_transition_mutex_; //!< State transition mutex
     };
 
+    //! IStateInfo - state information interface class
     struct IStateInfo
     {
+        // Returns the current state type value
         virtual PcoCameraState::StateType state_type(void) const = 0;
     };
+
+    //! State class definitions. These inherit from the boost statechart state base class
+    //! and implement the IStateInfo interface allowing the current state to be interrogated.
+    //! Each state class defines one or more custom reactions to legal transition events and
+    //! implements a react method for each of those transitions.
+
+    //! Disconnected - disconnected state class
+    //!
+    //! The disconnected state is the default state in the camera state machine, in which the
+    //! controller is not connected to the camera system.
+    //!
+    //! The following state transition events are supported:
+    //!    connect -> connected
 
     struct Disconnected : IStateInfo, sc::state<Disconnected, PcoCameraState>
     {
         public:
+            //! Defines reactions to legal events
             typedef sc::custom_reaction<EventConnect> reactions;
 
+            //! Constructor - initialises state machine context
             Disconnected(my_context ctx) : my_base(ctx) {};
 
+            //! Reacts to connect transition events
             sc::result react(const EventConnect&);
 
+            //! Returns the current state type
             PcoCameraState::StateType state_type(void) const
             {
                 return PcoCameraState::StateType::StateDisconnected;
             }
     };
 
+    //! Connected - connected state class
+    //!
+    //! The connected state is occupied when the controller is connected to the camera system
+    //! but the camera has not been configured and armed for image acquisition.
+    //!
+    //! The following state transition events are supported:
+    //!
+    //!    disconnect -> disconnected
+    //!    arm        -> armed
+
     struct Connected : IStateInfo, sc::state<Connected, PcoCameraState>
     {
         public:
+            //! Defines reactions to legal events
             typedef mpl::list<
                 sc::custom_reaction<EventDisconnect>,
                 sc::custom_reaction<EventArm>
             > reactions;
 
+            //! Constructor - initialises state machine context
             Connected(my_context ctx) : my_base(ctx) {};
 
+            //! Reacts to disconnect transition events
             sc::result react(const EventDisconnect&);
+            //! Reacts to arm transition events
             sc::result react(const EventArm&);
 
+            //! Returns the current state type
             PcoCameraState::StateType state_type(void) const
             {
                 return PcoCameraState::StateType::StateConnected;
@@ -167,34 +236,63 @@ namespace FrameReceiver
 
     };
 
+    //! Armed - armed state class
+    //!
+    //! The armed state is occupied when the controller has armed the camera system for image
+    //! acquisition, which commits configuration settings to the camera and prepares for image
+    //! acquisition.
+    //!
+    //! The following state transition events are supported:
+    //!
+    //!    disarm       -> connected
+    //!    record start -> recording
+
     struct Armed : IStateInfo, sc::state<Armed, PcoCameraState>
     {
         public:
-            typedef mpl::list<
+           //! Defines reactions to legal events
+           typedef mpl::list<
                 sc::custom_reaction<EventDisarm>,
                 sc::custom_reaction<EventRecordStart>
             > reactions;
 
+            //! Constructor - initialises state machine context
             Armed(my_context ctx) : my_base(ctx) {};
 
+            //! Reacts to disarm transition events
             sc::result react(const EventDisarm&);
+            //! Reacts to record start transition events
             sc::result react(const EventRecordStart&);
 
+            //! Returns the current state type
             PcoCameraState::StateType state_type(void) const
             {
                 return PcoCameraState::StateType::StateArmed;
             }
     };
 
+    //! Recording - recording state class
+    //!
+    //! The recording state is occupied when the controller has started the camera in recording
+    //! mode and images are being acquired.
+    //!
+    //! The following state transition events are supported:
+    //!
+    //!    record stop -> armed
+
     struct Recording : IStateInfo, sc::state<Recording, PcoCameraState>
     {
         public:
+            //! Defines reactions to legal events
             typedef sc::custom_reaction<EventRecordStop> reactions;
 
+            //! Constructor - initialises state machine context
             Recording(my_context ctx) : my_base(ctx) {};
 
+            //! Reacts to record stop transition events
             sc::result react(const EventRecordStop&);
 
+            //! Returns the current state type
             PcoCameraState::StateType state_type(void) const
             {
                 return PcoCameraState::StateType::StateRecording;
--- data/frameReceiver/src/PcoCameraStateMachine.cpp
+++ data/frameReceiver/src/PcoCameraStateMachine.cpp
@@ -1,3 +1,15 @@
+/*
+ * PcoCameraStateMachine.cpp - finite state machine implementation for the PCO camera controller
+ *
+ * This class implements a finite state machine handing for the PCO camera controller. This
+ * is based on the boost StartChart library. The camera state responds to known commands
+ * passed to the controller by a client and reacts accordingly, executing state transitions and
+ * triggering operations in the controller.
+ *
+ * Created on: 22 Sept 2021
+ *     Author: Tim Nicholls, STFC Detector Systems Group
+ */
+
 #include <iostream>
 #include <sstream>
 #include "PcoCameraStateMachine.h"
@@ -5,9 +17,17 @@
 
 using namespace FrameReceiver;
 
+//! Constructor for the PcoCameraState class.
+//!
+//! This constructor initialises the state machine, defining custom type information for the
+//! transition events.
+//!
+//! \param controller - pointer to the PCO controller instance, which acts on state transitions
+
 PcoCameraState::PcoCameraState(PcoCameraLinkController* controller) :
     controller_(controller)
 {
+    // Define custom type information for naming state transition events
     EventDisconnect::custom_static_type_ptr("disconnect");
     EventConnect::custom_static_type_ptr("connect");
     EventArm::custom_static_type_ptr("arm");
@@ -16,29 +36,58 @@ PcoCameraState::PcoCameraState(PcoCameraLinkController* controller) :
     EventRecordStop::custom_static_type_ptr("stop");
 }
 
+//! Executes a state transition command.
+//!
+//! This method executes the specified state transition command. Specified as a pointer to a char
+//! array, the command name is converted to a string and passed to the overloaded execute_command
+//! method.
+//!
+//! \param command - command name as a character array pointer
+
 void PcoCameraState::execute_command(const char* command)
 {
     std::string cmd_str(command);
     this->execute_command(cmd_str);
 }
 
+//! Executes a state transition command.
+//!
+//! This method executes the specified state transition command. Specified as a pointer to a char
+//! array, the command name is mapped to the command type value and passed to the overloaded
+//! execute_command method.
+//!
+//! \param command - command name as string reference
+
 void PcoCameraState::execute_command(std::string& command)
 {
+    // Map the specified command to a type value
     CommandType command_type = map_command_to_type(command);
 
+    // If the command is not recognised, throw an exception
     if (command_type == CommandUnknown)
     {
         std::stringstream ss;
         ss << "Unknown camera state transition command: " << command;
         throw(PcoCameraStateUnknownCommand(ss.str()));
     }
+
+    // Otherwise execute the command
     this->execute_command(command_type);
 }
 
+//! Executes a state transition command.
+//!
+//! This method executes the specified state transition command. THe command type specified as an
+//! argument is mapped to appropriate transition event, which is then procssed.
+//!
+//! \param command - state transition command type valuet
+
 void PcoCameraState::execute_command(PcoCameraState::CommandType command)
 {
+    // Acquire the transition lock to prevent concurrent access
     boost::lock_guard<boost::mutex> transition_lock(state_transition_mutex_);
 
+    // Process the appropriate transition event based on the command value
     switch (command)
     {
         case CommandConnect:
@@ -63,6 +112,7 @@ void PcoCameraState::execute_command(PcoCameraState::CommandType command)
             // Deliberate fall-through
         default:
             {
+                // If an illegal or unknown command value is specified, throw an exception
                 std::stringstream ss;
                 ss << "Unknown camera state transition command type: " << (int)command;
                 throw(PcoCameraStateUnknownCommand(ss.str()));
@@ -71,23 +121,45 @@ void PcoCameraState::execute_command(PcoCameraState::CommandType command)
     }
 }
 
-void PcoCameraState::unconsumed_event(const sc::event_base& e)
+//! Handles unconsumed, i.e. invalid, state transition events.
+//!
+//! This method is called when a state transition event is not consumed by the state machine, that
+//! is when an transition is requested that is not valid in the current state. An illegal transition
+//! exception is formatted with a relevent error string and thrown.
+//!
+//! \param event - reference to the unconsumed event
+
+void PcoCameraState::unconsumed_event(const sc::event_base& event)
 {
+    // Format an appropriate error message
     std::stringstream ss;
-    ss << e.custom_dynamic_type_ptr<char>() << " is not valid in "
+    ss << event.custom_dynamic_type_ptr<char>() << " is not valid in "
         << current_state_name() << " state";
+
+    // Throw an illegal transition exception
     throw(PcoCameraStateIllegalTransition(ss.str()));
 }
 
+//! Maps a command name string to a command type value
+//!
+//! This method maps a specified command name string to the appropriate command type, handling
+//! unknown strings by returning a value of CommandUnknown.
+//!
+//! \param command - reference to the command name as a string
+//! \return enumerated command type value
+
 PcoCameraState::CommandType PcoCameraState::map_command_to_type(std::string& command)
 {
+    // Set default return value
     CommandType command_type = CommandUnknown;
 
+    // Initialise the command type map if empty
     if (command_type_map_.size() == 0)
     {
         init_command_type_map();
     }
 
+    // If the specified command is present in the map, set the command type accordingly
     if (command_type_map_.left.count(command))
     {
         command_type = command_type_map_.left.at(command);
@@ -95,15 +167,26 @@ PcoCameraState::CommandType PcoCameraState::map_command_to_type(std::string& com
     return command_type;
 }
 
+//! Maps a state type to a state name.
+//!
+//! This methos maps an enumerated state type to a state name. Unknown state types return a
+//! string name of unknown.
+//!
+//! \param state_type - state type value
+//! \return state name as a string
+
 std::string PcoCameraState::map_state_to_name(StateType state_type)
 {
+    // Set default return value
     std::string state_name = "unknown";
 
+    // Initialise the command state map if empty
     if (state_type_map_.size() == 0)
     {
         init_state_type_map();
     }
 
+    // If the specified state is present in the map, set the state name accordingly
     if (state_type_map_.right.count(state_type))
     {
         state_name = state_type_map_.right.at(state_type);
@@ -111,15 +194,33 @@ std::string PcoCameraState::map_state_to_name(StateType state_type)
     return state_name;
 }
 
+// Returns the current state name.
+//!
+//! This method returns the current state name as a string.
+//!
+//! \return current state name as a string
+
 std::string PcoCameraState::current_state_name(void)
 {
     return map_state_to_name(current_state());
 }
 
+//! Returns the current state type.
+//!
+//! This method returns the current state type value by calling the state_type interface method
+//! on the current state object.
+//!
+//! \return current state type value
+
 PcoCameraState::StateType PcoCameraState::current_state(void)
 {
     return state_cast<const IStateInfo&>().state_type();
 }
+
+//! Initialises the command type map.
+//!
+//! This method initialises the entries in the command type map, which maps command names to
+//! enumerated command type values.
 
 void PcoCameraState::init_command_type_map(void)
 {
@@ -131,6 +232,11 @@ void PcoCameraState::init_command_type_map(void)
     command_type_map_.insert(CommandTypeMapEntry("stop",       CommandStopRecording));
 }
 
+//! Initialises the state type map.
+//!
+//! This method initialises the entries in the state type map, which maps state names to
+//! enumerated state type values.
+
 void PcoCameraState::init_state_type_map(void)
 {
     state_type_map_.insert(StateTypeMapEntry("disconnected", StateDisconnected));
@@ -139,11 +245,25 @@ void PcoCameraState::init_state_type_map(void)
     state_type_map_.insert(StateTypeMapEntry("recording",    StateRecording));
 }
 
+//! Reacts to connect transition events
+//!
+//! This method reacts to connect transition events in the disconnected state. The controller
+//! connect method is called and the state transits to connected.
+//!
+//! \param reference to connect event
+
 sc::result Disconnected::react(const EventConnect&)
 {
     outermost_context().controller_->connect();
     return transit<Connected>();
 }
+
+//! Reacts to disconnect transition events
+//!
+//! This method reacts to disconnect transition events in the connected state. The controller
+//! disconnect method is called and the state transits to disconnected.
+//!
+//! \param reference to disconnect event
 
 sc::result Connected::react(const EventDisconnect&)
 {
@@ -151,11 +271,25 @@ sc::result Connected::react(const EventDisconnect&)
     return transit<Disconnected>();
 }
 
+//! Reacts to arm transition events
+//!
+//! This method reacts to arm transition events in the connected state. The controller arm method
+//! is called and the state transits to armed.
+//!
+//! \param reference to connect event
+
 sc::result Connected::react(const EventArm&)
 {
     outermost_context().controller_->arm();
     return transit<Armed>();
 }
+
+//! Reacts to disarm transition events
+//!
+//! This method reacts to disarm transition events in the armed state. The controller disarm method
+//! is called and the state transits to connected.
+//!
+//! \param reference to connect event
 
 sc::result Armed::react(const EventDisarm&)
 {
@@ -163,11 +297,25 @@ sc::result Armed::react(const EventDisarm&)
     return transit<Connected>();
 }
 
+//! Reacts to record start transition events
+//!
+//! This method reacts to record state transition events in the conarmednected state. The controller
+//! start_recording method is called and the state transits to recording.
+//!
+//! \param reference to connect event
+
 sc::result Armed::react(const EventRecordStart&)
 {
     outermost_context().controller_->start_recording();
     return transit<Recording>();
 }
+
+//! Reacts to record stop transition events
+//!
+//! This method reacts to record stop transition events in the recording state. The controller
+//! stop_recording state is called and the state transits to armed.
+//!
+//! \param reference to connect event
 
 sc::result Recording::react(const EventRecordStop&)
 {
